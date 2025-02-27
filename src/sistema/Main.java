4ff7d419cdf7e4026c274d19de6afe6f33dfe635
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-02-27 14:45:32 -0300

    commit

--- src/sistema/Main.java
+++ src/sistema/Main.java
@@ -4,3 +4,9 @@ public class Produto {
     private int id;
     private String nome;
     private double preco;
+
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
