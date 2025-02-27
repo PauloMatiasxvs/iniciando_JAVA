9bf1478fea4984548fcf56e407068a21f9e0f6da
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-02-27 14:43:23 -0300

    commit

--- src/sistema/Main.java
+++ src/sistema/Main.java
@@ -1,8 +1,6 @@
 package sistema;
 
-public class Main {
-    
-    public static void main(String[] args) {
-        System.out.println("Agora vai funcionar!");
-    }
-}
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
