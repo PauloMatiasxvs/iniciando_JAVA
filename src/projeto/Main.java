378b02379e4d23d79add669afe865d5cd1cc9654
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-02-27 14:33:14 -0300

    commit

--- src/projeto/Main.java
+++ src/projeto/Main.java
@@ -1,5 +1,12 @@
-package projeto;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Meu primeiro programa Java!");
+        System.out.println("Demonstração de cálculo simples:");
+        
+        int numero1 = 15;
+        int numero2 = 5;
+        int resultado = numero1 + numero2;
+        
+        System.out.printf("%d + %d = %d", numero1, numero2, resultado);
+    }
 }
